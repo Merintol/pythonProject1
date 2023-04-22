217fa6715d6b0e3720c0803e3a8cf257bac533bc
smola <smolaaleksey@gmail.com>
2023-04-22 02:52:22 +0200

    posled

--- posled.py
+++ posled.py
@@ -9,10 +9,10 @@ class Encoder:
         return str(self.__num)
 
     def __encrypt(self):
-        # інкапсульований метод для шифрування числа
+
         self.__num = (self.__num * random.randint(1, 10)) % 100
 
     def get_result(self):
-        # метод, який виконує шифрування та випадкову математичну операцію
+
         self.__encrypt()
         return self.__num + random.randint(1, 10)
