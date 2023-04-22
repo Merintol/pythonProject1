9dd63960713e5e5cde9bf8b9d9948cfc57b3d0c7
smola <smolaaleksey@gmail.com>
2023-04-22 02:43:09 +0200

    dz2

---
+++ dz4.py
@@ -0,0 +1,10 @@
+import time
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Function {func.__name__} execution time: {end_time - start_time:.6f} seconds")
+        return result
+    return wrapper
